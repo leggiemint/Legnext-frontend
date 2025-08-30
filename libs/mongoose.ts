35e02ce2d0bdb21bf0430632df3071cbfe85fd9b
tobias-hui <huikai@tritonix.cn>
2025-08-31 01:16:37 +0800

    Fix TypeScript error: remove invalid bufferMaxEntries option

--- libs/mongoose.ts
+++ libs/mongoose.ts
@@ -23,7 +23,6 @@ const connectMongo = async () => {
     serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     bufferCommands: false, // Disable mongoose buffering
-    bufferMaxEntries: 0, // Disable mongoose buffering
   });
 
   try {
